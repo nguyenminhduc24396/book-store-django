8bb642c2006273be73d1e03ec5dee11f7cf90846
Nguyen Minh Duc <ducnm2@vmogroup.com>
2024-03-29 17:25:51 +0700

    aaaa

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -4,10 +4,17 @@ from django.utils.text import slugify
 
 # Create your models here.
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(validators=[MinValueValidator(1) , MaxValueValidator(5)])
-    author = models.CharField(null = True, max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
     is_bestselling = models.BooleanField(default=False)
     slug = models.SlugField(default="", null=False, db_index=True)
 
